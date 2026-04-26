642bb73bade3402b68906498189c9b5df761449f
Anoushkha1225 <vedaanoushkha@gmail.com>
2026-04-26 17:04:14 +0530

    v0.2.1 clone code changed

--- gitbuddy/commands/clone.py
+++ gitbuddy/commands/clone.py
@@ -1,3 +1,12 @@
+def clone(url: str, path: str = "."):
+    from gitbuddy.engine import GitEngine
+    from pathlib import Path
+    engine = GitEngine()
+    target = Path(path).resolve()
+    if path != "." and target.exists() and any(target.iterdir()):
+        print(f"Error: folder '{path}' already exists and is not empty.")
+        return
+    engine.run(f"git clone {url} {path}".strip())
 def connect(url: str):
     from gitbuddy.engine import GitEngine
     from rich.console import Console
